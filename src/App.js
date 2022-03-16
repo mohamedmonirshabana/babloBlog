447aa2186170b5c19e34ac8088b07cfa1322bd5c
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2022-03-16 14:59:28 +0200

    Example for many Data Types

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const title = 'Werlcome to the new Blog';
   const likes = 50;
   const person = { name: 'yoshi', age: 30 };
+  const googleLink = "http://www.google.com";
 
 
   return (
@@ -14,7 +15,11 @@ function App() {
         <h1>{title}</h1>
         <p>Liked {likes} times</p>
         {/* <p>{ person }</p> */}
-        <p>{ 10 }</p>
+        <p>{10}</p>
+        <p>{"hello, Ninja"}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p> {Math.random() * 20} </p>
+        <a href={ googleLink }>Google Site</a>
       </div>
     </div>
   );
