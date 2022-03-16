8532fde8b50d50636cd90547d7832eeb401b8680
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2022-03-16 16:51:53 +0200

    create Event For Button Click

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,13 @@
 const Home = () => {
+
+    const handleClick = () => { 
+        console.log('hello , ninjas');
+    };
+
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>click me</button>
         </div>
      );
 }
