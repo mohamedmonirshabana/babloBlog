e408aa431f17b7742e1ca4fbbf5de503d35c7432
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2022-03-16 17:23:18 +0200

    add another hook for use

--- src/Home.js
+++ src/Home.js
@@ -3,17 +3,19 @@ import { useState } from 'react';
 const Home = () => {
     // let name = 'mario';
     const [name, setName] = useState('mario');
+    const [age, setAge] = useState(22);
 
     const handleClick = () => { 
         // name = 'luigi';
         setName('luigi');
+        setAge(37);
     };
 
 
     return ( 
         <div className="home">
             <h2>Home Page</h2>
-            <p>{ name }</p>
+            <p>{name} is { age } years old</p>
             <button onClick={handleClick}>click me</button>
            
         </div>
