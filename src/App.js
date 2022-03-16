251d9b70488a5f9bcdf755abe4cb38a6f6a6a6c7
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2022-03-16 14:42:26 +0200

    react can't represent boolean , object Data Types

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import './App.css';
 function App() {
 
   const title = 'Werlcome to the new Blog';
-
+  const likes = 50;
+  const person = { name: 'yoshi', age: 30 };
 
 
   return (
     <div className="App">
       <div className="content">
-        <h1>{ title }</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        {/* <p>{ person }</p> */}
       </div>
     </div>
   );
