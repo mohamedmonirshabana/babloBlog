d0bbcd33ecc1e42718aefe7c5cf37c3ef20985e1
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2022-03-16 15:19:58 +0200

    Edit App Component For Work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
 
-  const title = 'Werlcome to the new Blog';
-  const likes = 50;
-  const person = { name: 'yoshi', age: 30 };
-  const googleLink = "http://www.google.com";
+ 
 
 
   return (
     <div className="App">
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        {/* <p>{ person }</p> */}
-        <p>{10}</p>
-        <p>{"hello, Ninja"}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p> {Math.random() * 20} </p>
-        <a href={ googleLink }>Google Site</a>
-      </div>
+        <h1>App Component</h1>
     </div>
   );
 }
