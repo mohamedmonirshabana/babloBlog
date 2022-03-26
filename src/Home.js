135c27ca444f0c4b121e963838736ba6a4d75f21
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2022-03-26 15:28:54 +0200

    Edit Code For made a request fro json file add add it to BlogList

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,31 @@
-import {  useEffect } from 'react';
-//useState,
-// import BlogList from './BlogList';
+import {  useState,useEffect } from 'react';
+//
+import BlogList from './BlogList';
 
 const Home = () => {
-    // const [blogs, setBlogs] = useState(null);
+    const [blogs, setBlogs] = useState(null);
 
    
 
-    // const handleDelete = (id) => { 
-    //     const newBlogs = blogs.filter(blog => blog.id !== id);
-    //     setBlogs(newBlogs);
-    // };
+    const handleDelete = (id) => { 
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    };
 
     useEffect(() => {
-        fetch('http://localhost:8000/Blogs')
+        fetch('http://localhost:8000/blogs')
             .then(res => {
                 return res.json();
             })
             .then(data => {
                 console.log(data);
+                setBlogs(data);
             });
     },[]);
     
     return ( 
         <div className="home">
-            {/* <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} /> */}
+            {blogs &&<BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />}
         </div>
      );
 }
