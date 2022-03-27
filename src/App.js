928240ded968055e5c67d813ce901d1b3b813b51
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2022-03-27 16:13:01 +0200

    add Link for Navbar

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Navbar/>
         <div className="content">
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
             </Route>
             <Route path="/create">
