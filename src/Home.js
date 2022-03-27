ff088b966ff743890a695af3dbd40b09b85f01c8
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2022-03-27 14:06:57 +0200

    add a custome hook for use it with many components and return result

--- src/Home.js
+++ src/Home.js
@@ -1,36 +1,10 @@
-import {  useState,useEffect } from 'react';
+// import {  useState,useEffect } from 'react';
 //
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogss')
-                .then(res => {
-                    console.log(res);
-                    if (!res.ok) {
-                        throw Error('could not fetch the data for that resource');
-                    }
-                    return res.json();
-                })
-                .then(data => {
-                    console.log(data);
-                    setBlogs(data);
-                    setIsPending(false);
-                    setError(null);
-                })
-                .catch((err) => {
-                    console.log(err.message);
-                    setIsPending(false);
-                    setError(err.message);
-                });
-        } ,1000);
-    },[]);
-    
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
     return ( 
         <div className="home">
             {error && <div>{error}</div>}
