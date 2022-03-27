d71cd9bfe9b79c2d456b52fa1e5d29193edacce9
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2022-03-27 13:15:22 +0200

    add error message for ui by useState

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
@@ -23,12 +24,14 @@ const Home = () => {
                 })
                 .catch((err) => {
                     console.log(err.message);
+                    setError(err.message);
                 });
         } ,1000);
     },[]);
     
     return ( 
         <div className="home">
+            {error && <div>{error}</div>}
             {isPending && <div>Loading....</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
