5c099e5bb754487a0aa4e969a0a6e8d8a1eb27b3
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2022-03-28 17:17:47 +0200

    add condition for Add to Fetch

--- src/Create.js
+++ src/Create.js
@@ -4,10 +4,13 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('');
+    const [isPending, setIsPending] = useState('false');
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const blog = { title, body, author };
+
+        setIsPending(true);
 
         fetch('http://localhost:8000/blogs', {
             method: 'POST',
@@ -15,6 +18,7 @@ const Create = () => {
             body: JSON.stringify(blog)
         }).then(() => {
             console.log('New Blog Add it');
+            setIsPending(false);
         })
 
         // console.log(blog);
@@ -46,7 +50,8 @@ const Create = () => {
                     <option value="Mohamed">Mohamed</option>
                     <option value="mario">mario</option>
                 </select>
-                <button>Add Blog</button>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding Blog...</button>}
                 {/* <p>{title}</p>
                 <p>{body}</p>
                 <p>{ author }</p> */}
