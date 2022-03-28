6145c02c11186bb1fcd7fd6edf047c58e2be4f05
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2022-03-28 02:58:12 +0200

    add how to create Blog by React

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,39 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
+    
     return ( 
         <div className="create">
             <h2>Add New Blog</h2>
+            <form>
+                <label>Blog Title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog Body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange ={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Medo">Medo</option>
+                    <option value="Mohamed">Mohamed</option>
+                    <option value="mario">mario</option>
+                </select>
+                <button>Add Blog</button>
+                <p>{ title }</p>
+            </form>
         </div>
      );
 }
