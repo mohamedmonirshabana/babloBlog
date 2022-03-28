b1b479aa5b8e3e8759de90e735efe72dc3417855
Mohamedmonirshabana <mohamed.monir.shabana@gmail.com>
2022-03-28 02:30:15 +0200

    add data for BlogDetals by hook fetch Data

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,6 @@
-const BlogList = ({blogs, title}) => {
+import { Link } from 'react-router-dom';
+
+const BlogList = ({ blogs, title }) => {
     // const blogs = props.blogs;
     // const title = props.title;
     // const handleDelete = (id) => { 
@@ -9,9 +11,11 @@ const BlogList = ({blogs, title}) => {
         <div className="blog-list">
             <h2>{ title }</h2>
              {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p> writen by : {blog.author}</p>
+                 <div className="blog-preview" key={blog.id}>
+                     <Link to={`/blogs/${blog.id}`}>
+                        <h2>{ blog.title }</h2>
+                        <p> writen by : {blog.author}</p>
+                     </Link>
                    
                 </div>
             ))}          
